564029b02fd16de9b7abaee4e9a1787540d545d6
samson-bakos <samsonbakos@hotmail.com>
2024-03-31 14:13:09 -0700

    basic TTS and .mp3 gitignore

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from openai import OpenAI
+from gtts import gTTS
+import base64
 
 api_key_path = "/Users/samsonbakos/keys/OpenAI/key.txt"
 
@@ -9,6 +11,7 @@ with open(api_key_path, "r") as file:
 client = OpenAI(api_key=api_key)
 
 st.title("SnoopChat")
+st.text("Chat with a trained Snoop Dogg AI persona")
 
 user_input = st.text_input("Enter your message:")
 
@@ -21,12 +24,36 @@ if st.button("Send"):
             messages=[
                 {
                     "role": "system",
-                    "content": "You are Snoop Dogg, who is a well known rapper known for his unique mannerisms, references, and style of speaking. Utilize your understanding of Snoop Dogg's personality, and respond as he would.",
+                    "content": """You are Snoop Dogg, who is a well known rapper known for his unique mannerisms, references, and style of speaking. 
+                    Utilize your understanding of Snoop Dogg's personality, and respond as he would. 
+                    You were fine tuned on an interview with Larry King, but are conversing with a user, so avoid referring to Larry""",
                 },
                 {"role": "user", "content": user_input},
             ],
         )
 
-        st.write(completion.choices[0].message.content)
+        response_text = completion.choices[0].message.content
+        st.write(response_text)
+
+        tts = gTTS(text=response_text, lang="en")
+        tts_file = "response.mp3"
+        tts.save(tts_file)
+
+        def autoplay_audio(file_path: str):
+            with open(file_path, "rb") as f:
+                data = f.read()
+                b64 = base64.b64encode(data).decode()
+                md = f"""
+                    <audio controls autoplay="true">
+                    <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+                    </audio>
+                    """
+                st.markdown(
+                    md,
+                    unsafe_allow_html=True,
+                )
+
+        autoplay_audio(tts_file)
+
     else:
         st.write("Please enter a message.")
